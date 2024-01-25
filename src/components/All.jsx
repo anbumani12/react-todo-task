e43663c4ff06dd837f6f5bf2f55c7ccff445f4c2
anbumani12 <anbumani15012002@gmail.com>
2024-01-25 13:07:32 +0530

    task completed

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
 
-const All = ({todos, setTodos}) => {
+const All = ({ todos, setTodos }) => {
   return (
-    <>     
-          {todos.map((e, i) => {
-            return <Card data={todos} todos={e} setTodos={setTodos} key={i}  />;
-        })}
-       
+    <>
+      {todos.map((e, i) => {
+        return <Card data={todos} todos={e} setTodos={setTodos} key={i} />;
+      })}
     </>
-  )
-}
+  );
+};
 
-export default All
+export default All;
